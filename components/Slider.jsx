e4388f29fb1b578658c38e11fab3db9e33656f49
Medwebdev <mtakisnani09@gmail.com>
2022-08-27 15:35:46 +0100

    Added Slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/dist/client/image'
 import React from 'react'
 import Container from './Container'
-
+import Sliderbg from '../public/assets/Sliderbg.webp'
 const Slider = () => {
   return (
     <div className='w-full relative py-10 h-full max-h-[500px]'>
-        <div className='bg absolute inset-0 opacity-60 z-[-1]'>
-            <Image src={'/../public/assets/sliderbg.webp'} layout='fill' alt='Slider Background' />
+        <div className='bg-gray-100 absolute inset-0 opacity-60 z-[-1]'>
+            <Image src={Sliderbg} layout='fill' alt='Slider Background' />
         </div>
         <Container>
             <div className='w-full h-full bg-white p-40'></div>
